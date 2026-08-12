3b4474caf03bd57b70830ec491a4287187fdcae6
myduckiinn <myduckiinn@gmail.com>
2026-08-12 12:27:27 -0400

    s commit

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-class Veiculo {
-    constructor(){
-        this.marca = [];
-        this.modelo = [];
-    }
-    ligar()
-}
+class Veiculo { 
+  constructor(marca, modelo){ 
+    this.marca = marca; 
+    this.modelo = modelo; 
+  } 
+  ligar(){ 
+    console.log(`${this.marca} ${this.modelo} foi ligado.`); 
+  } 
+  desligar(){ 
+    console.log(`${this.marca} ${this.modelo} foi desligado.`); 
+  } 
+} 
+
+class Onibus extends Veiculo { 
+  constructor(marca, modelo) { 
+    super(marca, modelo); 
+    this.quantidadePassageiros = 0; 
+    this.limiteMaximo = 30; 
+  } 
+  embarcar(quantidade) { 
+    let totalAposEmbarque = this.quantidadePassageiros + quantidade; 
+    if (totalAposEmbarque > this.limiteMaximo) { 
+      console.log(`Não pode embarcar. O limite é de 30 pessoas: ${totalAposEmbarque}`); 
+    } else { 
+      this.quantidadePassageiros = totalAposEmbarque; 
+      console.log(`Embarque realizado! Passageiros a bordo: ${this.quantidadePassageiros}`); 
+    } 
+  } 
+} 
+
+const meuOnibus = new Onibus("Mercedes", "Marcopolo"); 
+meuOnibus.ligar(); 
+meuOnibus.embarcar(40); 
+meuOnibus.desligar();
